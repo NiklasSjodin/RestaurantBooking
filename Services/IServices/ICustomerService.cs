cb93a1179c7c6978f196482a996fc060067d06c8
Niklas Sjödin <sjoodiin@hotmail.com>
2024-09-19 20:14:17 +0200

    Added CRUD operations

---
+++ Services/IServices/ICustomerService.cs
@@ -0,0 +1,15 @@
+﻿using RestaurantBooking.Models;
+using RestaurantBooking.Models.DTOs.Customer;
+
+namespace RestaurantBooking.Services.IServices
+
+{
+    public interface ICustomerService
+    {
+        Task<IEnumerable<GetCustomerDTO>> GetAllCustomersAsync();
+        Task<GetCustomerDTO> GetCustomerByIdAsync(int id);
+        Task AddCustomerAsync(CreateCustomerDTO createCustomer);
+        Task UpdateCustomerAsync(UpdateCustomerDTO updateCustomer);
+        Task DeleteCustomerAsync(int id);
+    }
+}
