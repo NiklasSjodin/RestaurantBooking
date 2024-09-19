bc0c85d3896aa209a1e55df663c16ab5aec564f1
Niklas Sjödin <sjoodiin@hotmail.com>
2024-09-19 20:08:49 +0200

    Added a DTO for creating an item in the menu

---
+++ Models/DTOs/MenuItem/CreateMenuItemDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace RestaurantBooking.Models.DTOs.MenuItem
+{
+    public class CreateMenuItemDTO
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
