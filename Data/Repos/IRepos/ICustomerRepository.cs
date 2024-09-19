7e1ec5e78b6af3073a63efbbdc7db22863af4a3b
Niklas Sjödin <sjoodiin@hotmail.com>
2024-09-19 20:11:39 +0200

    Added customer irepo with CRUD operations

---
+++ Data/Repos/IRepos/ICustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using RestaurantBooking.Models;
+
+namespace RestaurantBooking.Data.Repos.IRepos
+{
+    public interface ICustomerRepository
+    {
+        Task<IEnumerable<Customer>> GetAllCustomersAsync();
+        Task<Customer> GetCustomerByIdAsync(int id);
+        Task AddCustomerAsync(Customer customer);
+        Task UpdateCustomerAsync(Customer customer);
+        Task DeleteCustomerAsync(Customer customer);
+    }
+}
