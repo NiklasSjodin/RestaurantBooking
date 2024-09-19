04d9c94103d103dce62adf55ae7241dbaa6f2d2e
Niklas Sjödin <sjoodiin@hotmail.com>
2024-09-19 20:13:43 +0200

    Added a DTO for geting a table

---
+++ Models/DTOs/Table/GetTableDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace RestaurantBooking.Models.DTOs.Table
+{
+    public class GetTableDTO
+    {
+        public int TableID { get; set; }
+        public int TableNumber { get; set; }
+        public int NumberOfSeats { get; set; }
+    }
+}
