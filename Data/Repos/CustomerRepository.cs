635231fe83a41c21b7ee558ff7dd76d69a5dfccb
Niklas Sjödin <sjoodiin@hotmail.com>
2024-09-19 20:12:33 +0200

    Added CRUD operations for customersRepo

---
+++ Data/Repos/CustomerRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantBooking.Data.Repos.IRepos;
+using RestaurantBooking.Models;
+
+namespace RestaurantBooking.Data.Repos
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly RestaurantContext _context;
+        public CustomerRepository(RestaurantContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Customer>> GetAllCustomersAsync()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+        public async Task<Customer> GetCustomerByIdAsync(int id)
+        {
+            var customer = await _context.Customers.FirstOrDefaultAsync(c => c.CustomerID == id);
+
+            return customer;
+        }
+
+        public async Task AddCustomerAsync(Customer customer)
+        {
+            await _context.Customers.AddAsync(customer);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteCustomerAsync(Customer customer)
+        {
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateCustomerAsync(Customer customer)
+        {
+            _context.Customers.Update(customer);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
