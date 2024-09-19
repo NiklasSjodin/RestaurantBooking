c1338debc52c30db34df090d8e999d85ce5ede63
Niklas Sjödin <sjoodiin@hotmail.com>
2024-09-19 20:13:57 +0200

    Added a DTO for updating a table

---
+++ Models/DTOs/Table/UpdateTableDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace RestaurantBooking.Models.DTOs.Table
+{
+    public class UpdateTableDTO
+    {
+        public int TableID { get; set; }
+        public int TableNumber { get; set; }
+        public int NumberOfSeats { get; set; }
+    }
+}
