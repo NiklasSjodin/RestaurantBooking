294ce6021f96ef6ed568028e424c755cd95fa1d8
Niklas Sjödin <sjoodiin@hotmail.com>
2024-09-19 20:13:10 +0200

    Added our context

---
+++ Data/RestaurantContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantBooking.Models;
+
+namespace RestaurantBooking.Data
+{
+    public class RestaurantContext : DbContext
+    {
+        public RestaurantContext(DbContextOptions<RestaurantContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<MenuItem> MenuItems { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Table> Tables { get; set; }
+
+    }
+}
