6db2ef3c1ba121b6b5f96f8a5ee92574a731b0de
Niklas Sjödin <sjoodiin@hotmail.com>
2024-09-19 20:10:04 +0200

    Added a DTO for updating a reservation

---
+++ Models/DTOs/Reservation/UpdateReservationDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace RestaurantBooking.Models.DTOs.Reservation
+{
+    public class UpdateReservationDTO
+    {
+        public int ReservationId { get; set; }
+        public DateTime Time { get; set; }
+        public int NumberOfGuests { get; set; }
+    }
+}
