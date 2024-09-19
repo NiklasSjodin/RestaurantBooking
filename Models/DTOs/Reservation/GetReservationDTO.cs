be1546975a70caec7d6dd74f20a96611de71c9d7
Niklas Sjödin <sjoodiin@hotmail.com>
2024-09-19 20:09:52 +0200

    Added a DTO for geting a reservation

---
+++ Models/DTOs/Reservation/GetReservationDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantBooking.Models.DTOs.Reservation
+{
+    public class GetReservationDTO
+    {
+        public int ReservationId { get; set; }
+        public int CustomerID { get; set; }
+        public string FirstName { get; set; }
+        public DateTime Time { get; set; }
+        public int TableNumber { get; set; } 
+    }
+}
