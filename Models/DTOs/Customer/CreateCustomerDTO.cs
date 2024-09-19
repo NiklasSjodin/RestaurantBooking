ebdfcedac8e90b7c542ab4fc20317dc87d26f16d
Niklas Sjödin <sjoodiin@hotmail.com>
2024-09-19 20:07:39 +0200

    Added a DTO for creating a customer

---
+++ Models/DTOs/Customer/CreateCustomerDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantBooking.Models.DTOs.Customer
+{
+    public class CreateCustomerDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+}
