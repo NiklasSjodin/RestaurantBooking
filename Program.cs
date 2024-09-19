9197fdcefba57b0014a9c7270ce7a846bcb5cb0a
Niklas Sjödin <sjoodiin@hotmail.com>
2024-09-19 20:15:02 +0200

    Added DbContext, AddScoped for repos and services

--- Program.cs
+++ Program.cs
@@ -1,11 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using RestaurantBooking.Data;
+using RestaurantBooking.Data.Repos;
+using RestaurantBooking.Data.Repos.IRepos;
+using RestaurantBooking.Services;
+using RestaurantBooking.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<RestaurantContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
 
 var app = builder.Build();
 
