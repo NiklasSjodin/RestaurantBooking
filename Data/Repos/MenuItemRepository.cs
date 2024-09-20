7d1b1bca7edf68c8b23462127e09f783432e3cdd
Niklas Sjödin <sjoodiin@hotmail.com>
2024-09-20 11:55:25 +0200

    Implemented the CRUD operations with the following logic

--- Data/Repos/MenuItemRepository.cs
+++ Data/Repos/MenuItemRepository.cs
@@ -1,6 +1,43 @@
-﻿namespace RestaurantBooking.Data.Repos
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantBooking.Data.Repos.IRepos;
+using RestaurantBooking.Models;
+
+namespace RestaurantBooking.Data.Repos
 {
-    public class MenuItemRepository
+    public class MenuItemRepository : IMenuItemRepository
     {
+        private readonly RestaurantContext _context;
+        public MenuItemRepository(RestaurantContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<MenuItem>> GetAllMenuItemsAsync()
+        {
+            return await _context.MenuItems.ToListAsync();
+        }
+
+        public async Task<MenuItem> GetMenuItemByIdAsync(int id)
+        {
+            var menuItem = await _context.MenuItems.FirstOrDefaultAsync(mi => mi.ItemId == id);
+            return menuItem;
+        }
+        public async Task AddMenuItemAsync(MenuItem item)
+        {
+            await _context.MenuItems.AddAsync(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteMenuItemAsync(MenuItem item)
+        {
+            _context.MenuItems.Remove(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateMenuItemAsync(MenuItem item)
+        {
+            _context.MenuItems.Update(item);
+            await _context.SaveChangesAsync();
+        }
     }
 }
