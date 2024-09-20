f144d3c38f161f2fd3ec1e0e4e34fbfb505a6dcc
Niklas Sjödin <sjoodiin@hotmail.com>
2024-09-20 11:56:32 +0200

    Added so the endpoints worked and tested it, all good!

---
+++ Controllers/MenuItemsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantBooking.Models.DTOs.MenuItem;
+using RestaurantBooking.Services.IServices;
+
+namespace RestaurantBooking.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemsController : ControllerBase
+    {
+        private readonly IMenuItemService _menuItemService;
+        public MenuItemsController(IMenuItemService menuItemService)
+        {
+            _menuItemService = menuItemService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllMenuItems()
+        {
+            var menuItems = await _menuItemService.GetAllMenuItemsAsync();
+            return Ok(menuItems);
+        }
+
+        [HttpGet]
+        [Route("menuItem/{id}")]
+        public async Task<ActionResult> GetMenuItemById(int id)
+        {
+            var menuItem = await _menuItemService.GetMenuItemByIdAsync(id);
+            return Ok(menuItem);
+        }
+
+        [HttpPost]
+        [Route("createMenItem")]
+        public async Task<ActionResult> CreateMenuItem(CreateMenuItemDTO createMenuItem)
+        {
+            await _menuItemService.AddMenuItemAsync(createMenuItem);
+            return Ok("Menu Item created successfully!");
+        }
+
+        [HttpPut]
+        [Route("updateMenuItem/{id}")]
+        public async Task<ActionResult> UpdateMenuItem(int id, UpdateMenuItemDTO updateMenuItem)
+        {
+            await _menuItemService.UpdateMenuItemAsync(updateMenuItem);
+            return Ok("Menu Item updated!");
+        }
+        [HttpDelete]
+        [Route("deleteMenuItem/{id}")]
+        public async Task<ActionResult> DeleteMenuItem(int id)
+        {
+            await _menuItemService.DeleteMenuItemAsync(id);
+            return Ok("Menu Item deleted");
+        }
+    }
+}
