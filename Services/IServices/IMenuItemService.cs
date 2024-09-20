922b3aa44ea3b83fefc2eabe2c0ef7a434203afa
Niklas Sjödin <sjoodiin@hotmail.com>
2024-09-20 11:55:43 +0200

    Added CRUD operations for my service

---
+++ Services/IServices/IMenuItemService.cs
@@ -0,0 +1,14 @@
+﻿using RestaurantBooking.Models;
+using RestaurantBooking.Models.DTOs.MenuItem;
+
+namespace RestaurantBooking.Services.IServices
+{
+    public interface IMenuItemService
+    {
+        Task<IEnumerable<GetMenuItemDTO>> GetAllMenuItemsAsync();
+        Task<GetMenuItemDTO> GetMenuItemByIdAsync(int id);
+        Task AddMenuItemAsync(CreateMenuItemDTO createMenuItem);
+        Task UpdateMenuItemAsync(UpdateMenuItemDTO updateMenuItem);
+        Task DeleteMenuItemAsync(int id);
+    }
+}
