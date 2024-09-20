4c449f2a3d6948197379f2783e2709b16702cf29
Niklas Sjödin <sjoodiin@hotmail.com>
2024-09-20 11:54:50 +0200

    Added CRUD operations

--- Data/Repos/IRepos/IMenuItemRepository.cs
+++ Data/Repos/IRepos/IMenuItemRepository.cs
@@ -1,7 +1,13 @@
-﻿namespace RestaurantBooking.Data.Repos.IRepos
+﻿using RestaurantBooking.Models;
+
+namespace RestaurantBooking.Data.Repos.IRepos
 {
     public interface IMenuItemRepository
     {
-
+        Task<IEnumerable<MenuItem>> GetAllMenuItemsAsync();
+        Task<MenuItem> GetMenuItemByIdAsync(int id);
+        Task AddMenuItemAsync(MenuItem item);
+        Task UpdateMenuItemAsync(MenuItem item);
+        Task DeleteMenuItemAsync(MenuItem item);
     }
 }
