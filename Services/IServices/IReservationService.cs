b576cbaf4903be9512e9dceb707be18156b04468
Niklas Sjödin <sjoodiin@hotmail.com>
2024-09-24 12:23:16 +0200

    Created iservice for reservations with CRUD operations

---
+++ Services/IServices/IReservationService.cs
@@ -0,0 +1,14 @@
+﻿using RestaurantBooking.Models;
+using RestaurantBooking.Models.DTOs.Reservation;
+
+namespace RestaurantBooking.Services.IServices
+{
+    public interface IReservationService
+    {
+        Task<IEnumerable<GetReservationDTO>> GetAllReservationsAsync();
+        Task<GetReservationDTO> GetReservationByIdAsync(int id);
+        Task AddReservationAsync(CreateReservationDTO createReservation);
+        Task UpdateReservationAsync(UpdateReservationDTO updateReservation);
+        Task DeleteReservationAsync(int id);
+    }
+}
