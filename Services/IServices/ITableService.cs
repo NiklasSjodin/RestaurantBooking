19104e2663f2fe0d68977224b38154e6159668a2
Niklas Sjödin <sjoodiin@hotmail.com>
2024-09-24 11:30:07 +0200

    Created ITable service with CRUD operations

---
+++ Services/IServices/ITableService.cs
@@ -0,0 +1,13 @@
+﻿using RestaurantBooking.Models.DTOs.Table;
+
+namespace RestaurantBooking.Services.IServices
+{
+    public interface ITableService
+    {
+        Task<IEnumerable<GetTableDTO>> GetAllTablesAsync();
+        Task<GetTableDTO> GetTableByIdAsync(int id);
+        Task AddTableAsync(CreateTableDTO createTable);
+        Task UpdateTableAsync(UpdateTableDTO updateTable);
+        Task DeleteTableAsync(int id);
+    }
+}
