d401a2e9e91f23f2817db5d77eafe75dad803588
Niklas Sjödin <sjoodiin@hotmail.com>
2024-09-24 11:30:26 +0200

    Added functionallity to the CRUD operations

---
+++ Services/TableService.cs
@@ -0,0 +1,67 @@
+﻿using RestaurantBooking.Data.Repos.IRepos;
+using RestaurantBooking.Models;
+using RestaurantBooking.Models.DTOs.Table;
+using RestaurantBooking.Services.IServices;
+
+namespace RestaurantBooking.Services
+{
+    public class TableService : ITableService
+    {
+        private readonly ITableRepository _tableRepository;
+        public TableService(ITableRepository tableRepo)
+        {
+            _tableRepository = tableRepo;
+        }
+
+        public async Task<IEnumerable<GetTableDTO>> GetAllTablesAsync()
+        {
+            var tables = await _tableRepository.GetAllTablesAsync();
+            var tablesList = tables.Select(t => new GetTableDTO
+            {
+                TableID = t.TableID,
+                TableNumber = t.TableNumber,
+                NumberOfSeats = t.NumberOfSeats,
+            }).ToList();
+
+            return tablesList;
+        }
+
+        public async Task<GetTableDTO> GetTableByIdAsync(int id)
+        {
+            var foundTable = await _tableRepository.GetTableByIdAsync(id);
+            var table = new GetTableDTO
+            {
+                TableID = foundTable.TableID,
+                TableNumber = foundTable.TableNumber,
+                NumberOfSeats = foundTable.NumberOfSeats,
+            };
+            return table;
+        }
+        public async Task AddTableAsync(CreateTableDTO createTable)
+        {
+            var newTable = new Table
+            {
+                TableNumber = createTable.TableNumber,
+                NumberOfSeats = createTable.NumberOfSeats,
+            };
+            await _tableRepository.AddTableAsync(newTable);
+        }
+
+        public async Task DeleteTableAsync(int id)
+        {
+            var tableFound = await _tableRepository.GetTableByIdAsync(id);
+            await _tableRepository.DeleteTableAsync(tableFound);
+        }
+
+        public async Task UpdateTableAsync(UpdateTableDTO updateTable)
+        {
+            var table = await _tableRepository.GetTableByIdAsync(updateTable.TableID);
+
+            table.TableNumber = updateTable.TableNumber;
+            table.NumberOfSeats = updateTable.NumberOfSeats;
+
+            await _tableRepository.UpdateTableAsync(table);
+
+        }
+    }
+}
