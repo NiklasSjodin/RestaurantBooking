f42a81969236d30e706d9c84423e7d3e3d502372
Niklas Sjödin <sjoodiin@hotmail.com>
2024-09-24 12:22:22 +0200

    Created repo for reservations with CRUD operations

---
+++ Data/Repos/IRepos/IReservationRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantBooking.Models;
+
+namespace RestaurantBooking.Data.Repos.IRepos
+{
+    public interface IReservationRepository
+    {
+        Task<IEnumerable<Reservation>> GetAllReservationsAsync();
+        Task<Reservation> GetReservationByIdAsync(int id);
+        Task AddReservationAsync(Reservation reservation);
+        Task UpdateReservationAsync(Reservation reservation);
+        Task DeleteReservationAsync(Reservation reservation);
+    }
+}
