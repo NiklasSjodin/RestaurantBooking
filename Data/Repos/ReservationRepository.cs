9704bf6a89bac1ed57c8a658c6e5b9acaa835aad
Niklas Sjödin <sjoodiin@hotmail.com>
2024-09-24 12:22:52 +0200

    Added functionallity to the CRUD operations

---
+++ Data/Repos/ReservationRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantBooking.Data.Repos.IRepos;
+using RestaurantBooking.Models;
+
+namespace RestaurantBooking.Data.Repos
+{
+    public class ReservationRepository : IReservationRepository
+    {
+        private readonly RestaurantContext _context;
+        public ReservationRepository(RestaurantContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Reservation>> GetAllReservationsAsync()
+        {
+            return await _context.Reservations
+                .Include(r => r.Customer)
+                .Include(r => r.Table)
+                .ToListAsync();
+        }
+
+        public async Task<Reservation> GetReservationByIdAsync(int id)
+        {
+            var reservation = await _context.Reservations
+                .Include(r => r.Customer)
+                .Include(r => r.Table)
+                .FirstOrDefaultAsync(r => r.ReservationId == id);
+
+            return reservation;
+        }
+        public async Task AddReservationAsync(Reservation reservation)
+        {
+            await _context.Reservations.AddAsync(reservation);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteReservationAsync(Reservation reservation)
+        {
+            _context.Reservations.Remove(reservation);
+           await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateReservationAsync(Reservation reservation)
+        {
+            _context.Reservations.Update(reservation);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
