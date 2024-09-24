a74465f53e794ecdaad0a9c2fb981859d1fdb142
Niklas Sjödin <sjoodiin@hotmail.com>
2024-09-24 11:29:28 +0200

    Created TableRepo with CRUD operations

---
+++ Data/Repos/IRepos/ITableRepository.cs
@@ -0,0 +1,13 @@
+﻿using RestaurantBooking.Models;
+
+namespace RestaurantBooking.Data.Repos.IRepos
+{
+    public interface ITableRepository
+    {
+        Task<IEnumerable<Table>>GetAllTablesAsync();
+        Task<Table> GetTableByIdAsync(int id);
+        Task AddTableAsync(Table table);
+        Task UpdateTableAsync(Table table);
+        Task DeleteTableAsync(Table table);
+    }
+}
