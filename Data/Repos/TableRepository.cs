799a46657ed1b64f7091090d8ee92144cd5dd856
Niklas Sjödin <sjoodiin@hotmail.com>
2024-09-24 11:29:47 +0200

    Added functionallity to the CRUD operations

---
+++ Data/Repos/TableRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantBooking.Data.Repos.IRepos;
+using RestaurantBooking.Models;
+
+namespace RestaurantBooking.Data.Repos
+{
+    public class TableRepository : ITableRepository
+    {
+        private readonly RestaurantContext _context;
+        public TableRepository(RestaurantContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Table>> GetAllTablesAsync()
+        {
+           return await _context.Tables.ToListAsync();
+        }
+
+        public Task<Table> GetTableByIdAsync(int id)
+        {
+            var table = _context.Tables.FirstOrDefaultAsync(t => t.TableID == id);
+            return table;
+        }
+
+        public async Task AddTableAsync(Table table)
+        {
+            await _context.Tables.AddAsync(table);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteTableAsync(Table table)
+        {
+            _context.Tables.Remove(table);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateTableAsync(Table table)
+        {
+            _context.Tables.Update(table);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
