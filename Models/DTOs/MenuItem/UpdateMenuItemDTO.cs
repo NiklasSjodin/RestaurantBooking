929512a87b78a2a310e9c8420f8a80958832cc3e
Niklas Sjödin <sjoodiin@hotmail.com>
2024-10-02 15:49:45 +0200

    Added description and type of meal

--- Models/DTOs/MenuItem/UpdateMenuItemDTO.cs
+++ Models/DTOs/MenuItem/UpdateMenuItemDTO.cs
@@ -4,7 +4,9 @@
     {
         public int ItemId { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public double Price { get; set; }
+        public string TypeOf { get; set; }
         public bool IsAvailable { get; set; }
     }
 }
