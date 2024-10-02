ad47625afc02f9dd722827cd1745221b36fcfed3
Niklas Sjödin <sjoodiin@hotmail.com>
2024-10-02 15:49:19 +0200

    Added description and type of meal

--- Models/DTOs/MenuItem/CreateMenuItemDTO.cs
+++ Models/DTOs/MenuItem/CreateMenuItemDTO.cs
@@ -3,7 +3,9 @@
     public class CreateMenuItemDTO
     {
         public string Name { get; set; }
+        public string Description { get; set; }
         public double Price { get; set; }
+        public string TypeOf { get; set; }
         public bool IsAvailable { get; set; }
     }
 }
