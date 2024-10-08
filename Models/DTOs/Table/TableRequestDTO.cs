4605b73a035cb23d878a8b6cafbb4a2fc1e2da54
Niklas Sjödin <sjoodiin@hotmail.com>
2024-10-09 01:38:02 +0200

    Created a table request dto

---
+++ Models/DTOs/Table/TableRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace RestaurantBooking.Models.DTOs.Table
+{
+    public class TableRequestDTO
+    {
+        public DateTime ReservationDate { get; set; }
+        public int NumberOfGuests { get; set; }
+    }
+}
