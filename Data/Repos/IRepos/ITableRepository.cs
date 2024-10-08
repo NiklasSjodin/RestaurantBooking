4bc3703b0371709ac07245e538d25bc34ced0080
Niklas Sjödin <sjoodiin@hotmail.com>
2024-10-09 01:36:07 +0200

    Added the function availableTables

--- Data/Repos/IRepos/ITableRepository.cs
+++ Data/Repos/IRepos/ITableRepository.cs
@@ -5,6 +5,7 @@ namespace RestaurantBooking.Data.Repos.IRepos
     public interface ITableRepository
     {
         Task<IEnumerable<Table>>GetAllTablesAsync();
+        Task<IEnumerable<Table>>GetAvailableTablesAsync(DateTime reservationDate, int numberOfGuests);
         Task<Table> GetTableByIdAsync(int id);
         Task AddTableAsync(Table table);
         Task UpdateTableAsync(Table table);
