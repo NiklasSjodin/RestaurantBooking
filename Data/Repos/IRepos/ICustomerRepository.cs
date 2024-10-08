1f04597744e58c296e31b518c5eeaca3568980a4
Niklas Sjödin <sjoodiin@hotmail.com>
2024-10-09 01:35:48 +0200

    Changed so return a customer for getting the id in frontend

--- Data/Repos/IRepos/ICustomerRepository.cs
+++ Data/Repos/IRepos/ICustomerRepository.cs
@@ -6,7 +6,7 @@ namespace RestaurantBooking.Data.Repos.IRepos
     {
         Task<IEnumerable<Customer>> GetAllCustomersAsync();
         Task<Customer> GetCustomerByIdAsync(int id);
-        Task AddCustomerAsync(Customer customer);
+        Task<Customer> AddCustomerAsync(Customer customer);
         Task UpdateCustomerAsync(Customer customer);
         Task DeleteCustomerAsync(Customer customer);
     }
